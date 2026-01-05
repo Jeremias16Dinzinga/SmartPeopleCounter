71161603ca18d56a4dff59c0ea40e0dfb9b42dd8
Jeremias Dinzinga <117875623+Jeremias16Dinzinga@users.noreply.github.com>
2026-01-05 21:46:48 +0000

    Automatically save to the database

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://user:password@localhost/smart_people_counterdb"
+DATABASE_URL = "mysql+pymysql://root@localhost/smart_people_counterdb"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
